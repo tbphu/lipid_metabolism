148bb57f4669c41cba2dd9fef434e30562ab552c
Jens Hahn <jens.hahn@hu-berlin.de>
2016-04-14 17:02:18 +0200

    deleted uncalled functions and lists (number_xxx)

--- model.py
+++ model.py
@@ -160,42 +160,6 @@ class Model:
 
         # list for calculation of the lipid ratios in every membrane (goes to comp_ratio_dict)
         self.comp_ratio_list = []
-        # collecting the products of every time step. Lipids that are produced in the start function
-        # don't need the 0 for plotting
-        self.number_acetyl_coa = []
-        self.number_acyl_coa = []
-        self.number_pa = []
-        self.number_cdp_dg = []
-        self.number_tag = []
-        self.number_PS = []
-        self.number_PI = []
-        self.number_PE = []
-        self.number_PC = []
-        self.number_CL = []
-        self.number_ergosterol = []
-        self.number_sterylester = []
-        self.number_DAG = []
-        self.number_sphingolipid = []
-        # list of lists
-        self.number_lipids_list = [self.number_acyl_coa, self.number_pa, self.number_cdp_dg, self.number_tag,
-                                   self.number_PS, self.number_PI, self.number_PE, self.number_PC, self.number_CL,
-                                   self.number_ergosterol, self.number_sterylester, self.number_DAG,
-                                   self.number_sphingolipid]
-        # counting the lipids in each membrane after every time step
-        self.number_plasma_membrane = []
-        self.number_secretory_vesicles = []
-        self.number_vacuoles = []
-        self.number_nucleus = []
-        self.number_peroxisomes = []
-        self.number_light_microsomes = []
-        self.number_inner_mit_membrane = []
-        self.number_outer_mit_membrane = []
-        self.number_lipid_droplets = []
-        # list of lists
-        self.number_membranes_list = [self.number_plasma_membrane, self.number_secretory_vesicles, self.number_vacuoles,
-                                      self.number_nucleus, self.number_peroxisomes, self.number_light_microsomes,
-                                      self.number_inner_mit_membrane, self.number_outer_mit_membrane,
-                                      self.number_lipid_droplets]
 
         # possible fatty acids and weights to reach the biological proportions
         self.chainlength_saturated = {16: 'C16:0', 18: 'C18:0'}
@@ -1198,72 +1162,6 @@ class Model:
                                                  'C16:1': float(self.c16_1_sn2 + self.c16_1_sn1) / self.total_fatty_acids,
                                                  'C18:0': float(self.c18_0_sn2 + self.c18_0_sn1) / self.total_fatty_acids,
                                                  'C18:1': float(self.c18_1_sn2 + self.c18_1_sn1) / self.total_fatty_acids}
-
-    def plot_precursors(self):
-        """
-        Plotting the precursor molecules from the precursors_dict.
-        """
-        fig = mat.figure()
-        ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-        i = 0
-        while i < 6:  # in range(len(self.precursors.keys())):
-            ax.plot(self.t, self.precursors[self.precursors.keys()[i]], label=self.precursors.keys()[i])
-            i += 1
-        ax.plot(self.t, self.precursors['inositol'], label='inositol')
-        ax.plot(self.t, self.precursors['serine'], label='serine')
-        ax.plot(self.t, self.precursors['CTP'], label='CTP')
-        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        mat.show()
-
-    def plot_lipids(self):
-        """
-        Plotting the produced lipids before they are transported into the membranes
-        """
-        fig = mat.figure()
-        ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-        ax.plot(self.t, self.number_tag, label='tag')
-        ax.plot(self.t, self.number_PS, label='ps')
-        ax.plot(self.t, self.number_PI, label='pi')
-        ax.plot(self.t, self.number_PE, label='pe')
-        ax.plot(self.t, self.number_PC, label='pc')
-        ax.plot(self.t, self.number_CL, label='cl')
-        ax.plot(self.t, self.number_ergosterol, label='es')
-        ax.plot(self.t, self.number_sterylester, label='se')
-        ax.plot(self.t, self.number_sphingolipid, label='sl')
-        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        mat.show()
-
-    def plot_precursor_lipids(self):
-        """
-        Plotting some free precursor molecules.
-        """
-        fig = mat.figure()
-        ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-        ax.plot(self.t, self.number_acetyl_coa[:-1], label='acetyl_coa')
-        ax.plot(self.t, self.number_acyl_coa[:-1], label='acyl_coa')
-        ax.plot(self.t, self.number_pa[:-1], label='pa')
-        ax.plot(self.t, self.number_cdp_dg[:-1], label='cdp-dg')
-        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        mat.show()
-
-    def plot_membranes(self):
-        """
-        Plotting the number of lipids in the membranes of different compartments.
-        """
-        fig = mat.figure()
-        ax = fig.add_axes([0.1, 0.1, 0.6, 0.75])
-        ax.plot(self.t, self.number_plasma_membrane, label='plasma membrane')
-        ax.plot(self.t, self.number_secretory_vesicles, label='secretory vesicles')
-        ax.plot(self.t, self.number_vacuoles, label='vacuoles')
-        ax.plot(self.t, self.number_nucleus, label='nucleus')
-        ax.plot(self.t, self.number_peroxisomes, label='peroxisomes')
-        ax.plot(self.t, self.number_light_microsomes, label='light_microsomes')
-        ax.plot(self.t, self.number_inner_mit_membrane, label='inner_mit_membrane')
-        ax.plot(self.t, self.number_outer_mit_membrane, label='outer_mit_membrane')
-        ax.plot(self.t, self.number_lipid_droplets, label='lipid_droplets')
-        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-        mat.show()
-
 
 if __name__ == '__main__':
     # Test run, with runtime tracker
