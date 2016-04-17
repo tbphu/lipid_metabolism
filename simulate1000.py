0b107716d9029b06be3ec274963196017e135fea
Jens Hahn <jens.hahn@hu-berlin.de>
2016-04-18 01:22:14 +0200

    updated simulate1000, recursive call to ignore problems

--- simulate1000.py
+++ simulate1000.py
@@ -1,15 +1,27 @@
 import model
 
-for i in range(1000):
 
+def run_model():
     m = model.Model()
     # test run: 5 sec
     try:
         r, mem, s = m.run()
+        return r, mem, s
     except:
-        r, mem, s = m.run()
+        print 'fault'
+        run_model()
 
+i = 0
+for i in range(1000):
+    i += 1
+    print i
+    r, mem, s = run_model()
     for membrane in mem:
         with open('./' + membrane + '.txt', 'a') as lipid_file:
             line = str(mem[membrane]).strip('[').strip(']') + '\n'
             lipid_file.write(line)
+
+
+
+
+
