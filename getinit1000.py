62d022176920f439a0f410aafc58da42306e297e
Jens Hahn <jens.hahn@hu-berlin.de>
2016-04-19 01:15:18 +0200

    sim script updated

--- getinit1000.py
+++ getinit1000.py
@@ -6,11 +6,11 @@ for i in range(10):
     i += 1
     print i
     r, mem, s = m.run(0)
-    for membrane in mem:
+    for membrane in m.membranes_state:
         with open('./' + membrane + '.txt', 'a') as lipid_file:
-            line = str(mem[membrane]).strip('[').strip(']') + '\n'
+            line = str(len(m.membranes_state[membrane])) + '\n'
             lipid_file.write(line)
     for component in m.components_state:
         with open('./' + component + '.txt', 'a') as comp_file:
-            line = str(m.components_state[component]).strip('[').strip(']') + '\n'
+            line = str(len(m.components_state[component])) + '\n'
             comp_file.write(line)
