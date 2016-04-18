f6e73fc995ee79ba99badfe2a69a2c29ee6a509b
Jens Hahn <jens.hahn@hu-berlin.de>
2016-04-18 21:18:32 +0200

    minor

--- getinit1000.py
+++ getinit1000.py
@@ -5,17 +5,12 @@ for i in range(10):
     m = model.Model()
     i += 1
     print i
-    r, mem, s = m.model()
+    r, mem, s = m.run(0)
     for membrane in mem:
         with open('./' + membrane + '.txt', 'a') as lipid_file:
             line = str(mem[membrane]).strip('[').strip(']') + '\n'
             lipid_file.write(line)
     for component in m.components_state:
         with open('./' + component + '.txt', 'a') as comp_file:
-            line = str(mem[component]).strip('[').strip(']') + '\n'
+            line = str(m.components_state[component]).strip('[').strip(']') + '\n'
             comp_file.write(line)
-
-
-
-
-
